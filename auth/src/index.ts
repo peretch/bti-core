24019e182d0d76f459506db6b1563bec544d8ea2
peretch <sebastian@peretch.com>
2022-04-24 11:23:34 -0300

    App and index separated

--- auth/src/index.ts
+++ auth/src/index.ts
@@ -1,43 +1,14 @@
-import express from 'express';
-import 'express-async-errors';
-import { json } from 'body-parser';
 import mongoose from 'mongoose';
-import cookieSession from 'cookie-session';
 
-import { currentUserRouter } from './routes/current-user';
-import { signinRouter } from './routes/signin';
-import { signoutRouter } from './routes/signout';
-import { signupRouter } from './routes/signup';
+import { app } from './app';
 
-import { errorHandler } from './middlewares/error-handler';
-import { NotFoundError } from './errors/not-found-error';
 import { DatabaseConnectionError } from './errors/database-connection-error';
-
-const app = express();
-// Exrpess is aware that is behind a proxy (nginx ingress)
-app.set('trust proxy', true);
-app.use(json());
-app.use(
-  cookieSession({
-    signed: false,
-    secure: true,
-  })
-);
-
-app.use(currentUserRouter);
-app.use(signinRouter);
-app.use(signoutRouter);
-app.use(signupRouter);
-
-app.all('*', async (req, res) => {
-  throw new NotFoundError();
-});
-app.use(errorHandler);
 
 const start = async () => {
   if (!process.env.JWT_KEY) {
     throw new Error('JWT_KEY must be defined');
   }
+
   try {
     await mongoose.connect('mongodb://auth-mongo-srv:27017/auth');
     console.log('Connected to mongodb');
@@ -45,6 +16,7 @@ const start = async () => {
     console.error(err);
     throw new DatabaseConnectionError();
   }
+
   app.listen(3000, () => {
     console.log('Listening on port 3000!');
   });
