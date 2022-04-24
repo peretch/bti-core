40f5b2ed8b1546e0f6b3bf753730063d02283f56
peretch <sebastian@peretch.com>
2022-04-24 12:17:10 -0300

    Added test configuration and first test (signup)

---
+++ auth/src/routes/__test__/signup.test.ts
@@ -0,0 +1,76 @@
+import request from 'supertest';
+import { app } from '../../app';
+
+it('returns a 201 on successful signup', async () => {
+  return request(app)
+    .post('/api/users/signup')
+    .send({
+      email: 'test@test.com',
+      password: 'password',
+    })
+    .expect(201);
+});
+
+it('returns 400 with invalid email', async () => {
+  return request(app)
+    .post('/api/users/signup')
+    .send({
+      email: 'testtest.com',
+      password: 'password',
+    })
+    .expect(400);
+});
+
+it('returns 400 with invalid password', async () => {
+  return request(app)
+    .post('/api/users/signup')
+    .send({
+      email: 'test@test.com',
+      password: 'pas',
+    })
+    .expect(400);
+});
+
+it('returns 400 with missing email and/or password', async () => {
+  await request(app)
+    .post('/api/users/signup')
+    .send({ email: 'test@test.com' })
+    .expect(400);
+  await request(app)
+    .post('/api/users/signup')
+    .send({ password: 'password' })
+    .expect(400);
+  await request(app).post('/api/users/signup').send({}).expect(400);
+});
+
+it('disallaws duplicated emails', async () => {
+  await request(app)
+    .post('/api/users/signup')
+    .send({
+      email: 'test@test.com',
+      password: 'password',
+    })
+    .expect(201);
+  return request(app)
+    .post('/api/users/signup')
+    .send({
+      email: 'test@test.com',
+      password: 'password',
+    })
+    .expect(400);
+});
+
+it('sets cookie header after successful signup', async () => {
+  const response = await request(app)
+    .post('/api/users/signup')
+    .send({
+      email: 'test@test.com',
+      password: 'password',
+    })
+    .expect(201);
+
+  // As reminder, we are using `secure: true` when we define cookieSession middleware in our express app.
+  // This means that this cookie will only be visibled between HTTPS requests.
+  // If you want to be the solution, go to app.ts, and see the cookieSession middleware params object
+  expect(response.get('Set-Cookie')).toBeDefined();
+});
