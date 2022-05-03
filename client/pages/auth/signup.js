dd2c8f837bd740b4825d51f87b2251c3a8c067cc
Sebastián Pérez Etchandy <sebastian@peretch.com>
2022-05-03 19:58:02 -0300

    Aded useRequest hook

--- client/pages/auth/signup.js
+++ client/pages/auth/signup.js
@@ -1,22 +1,25 @@
 import { useState } from 'react';
-import axios from 'axios';
+import Router from 'next/router';
+
+import useRequest from '../../hooks/use-request';
 
 const Signup = () => {
   const [email, setEmail] = useState('');
   const [password, setPassword] = useState('');
-  const [errors, setErrors] = useState([]);
+  const { doRequest, errors } = useRequest({
+    url: '/api/users/signup',
+    method: 'post',
+    body: {
+      email,
+      password,
+    },
+    onSuccess: () => Router.push('/'),
+  });
 
   const onSubmit = async (event) => {
     event.preventDefault();
 
-    try {
-      const response = await axios.post('/api/users/signup', {
-        email,
-        password,
-      });
-    } catch (err) {
-      setErrors(err.response.data.errors);
-    }
+    doRequest();
   };
 
   return (
@@ -39,17 +42,10 @@ const Signup = () => {
           className="form-control"
         />
       </div>
-      {errors && errors.length > 0 && (
-        <div className="alert alert-danger">
-          <h4>Validation errors</h4>
-          <ul className="my-0">
-            {errors.map((error) => (
-              <li key={error.message}>{error.message}</li>
-            ))}
-          </ul>
-        </div>
-      )}
-      <button className="btn btn-primary">Sign Up</button>
+      {errors}
+      <div className="form-group mt-2">
+        <button className="btn btn-primary">Sign Up</button>
+      </div>
     </form>
   );
 };
--- client/pages/index.js
+++ client/pages/index.js
@@ -1,5 +1,14 @@
 const HomePage = () => {
-  return <h1>Big Ticket Items!</h1>;
+  return (
+    <div>
+      <h1>Big Ticket Items!</h1>
+      <ul>
+        <li>
+          <a href="/auth/signup">Login</a>
+        </li>
+      </ul>
+    </div>
+  );
 };
 
 export default HomePage;
