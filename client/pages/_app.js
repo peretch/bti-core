f237910336ce09ba1eb055baebabd8f7a8494dfc
Sebastián Pérez Etchandy <sebastian@peretch.com>
2022-05-03 22:11:01 -0300

    currentUser added from _app file. getInitialProps manually called if renderer copmponent contains definition:

--- client/pages/_app.js
+++ client/pages/_app.js
@@ -1,7 +1,40 @@
+import buildClient from '../api/build-client';
+
 import 'bootstrap/dist/css/bootstrap.css';
 
-const App = ({ Component, pageProps }) => {
-  return <Component {...pageProps} />;
+const AppComponent = ({ Component, pageProps, currentUser }) => {
+  return (
+    <div>
+      {currentUser.email}
+      <Component {...pageProps} currentUser={currentUser} />
+    </div>
+  );
 };
 
-export default App;
+// `getInitialProps` enables server-side rendering in a page and allows you to do initial data population,
+// it means sending the page with the data already populated from the server. This is especially useful for SEO.
+// ? This method is called from server only when user makes refresh, or clicking link, or typing Ultrasonic
+// ? If this component is loaded from navigation, this function will be executed on the application (For example, after login)
+
+// ! This getInitialProsp does not receives context with the same format than when we are un _app.js file in NextJS
+// AppComponent.getInitialProps = async (context) => {
+
+AppComponent.getInitialProps = async (appContext) => {
+  const client = buildClient(appContext.ctx);
+  const { data } = await client.get('/api/users/currentuser').catch((err) => {
+    console.log(err.message);
+  });
+  let pageProps = {};
+  if (appContext.Component.getInitialProps) {
+    pageProps = await appContext.Component.getInitialProps(appContext.ctx);
+  }
+
+  const { currentUser } = data;
+
+  return {
+    pageProps,
+    currentUser,
+  };
+};
+
+export default AppComponent;
