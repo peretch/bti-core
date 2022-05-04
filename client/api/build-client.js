7f93cda0ddd90bd12ee2fcd875d54dee6f7f45d3
Sebastián Pérez Etchandy <sebastian@peretch.com>
2022-05-03 21:39:10 -0300

    Implemented buildClient method in getInitialProps function in HomePage component

---
+++ client/api/build-client.js
@@ -0,0 +1,26 @@
+import axios from 'axios';
+
+// Here we will make use of the defined rules in ingress-srv.yaml to fetch data from services
+// We need to extract the Cookie of the Request, and forward it to the Auth services through the Ingress Controller
+// If we want to reach for example directly the Auth service, since they are in the same Namespace, we can do it with URL http://auth-srv
+// In this case, we CAN'T access ingress controller like ingress-server, because that pod is in OTHER Namespace
+// You can use `kubectl get namespaces` to see all existent Namespaces, you will see `default` and `ingress-nginx` as separated Namespaces
+// With command `kubectl get services -n ingress-nginx` we will see that the name of servie is ingress-nginx-controller
+// Example URL: `http://ingress-nginx.ingress-nginx-controller.svc.cluster.local/api/users/currentUser`
+
+const buildClient = ({ req }) => {
+  if (typeof window === 'undefined') {
+    // We are on server
+    return axios.create({
+      baseURL:
+        'http://ingress-nginx-controller.ingress-nginx.svc.cluster.local/api/users/currentuser',
+      headers: req.headers,
+    });
+  } else {
+    return axios.create({
+      baseURL: '/',
+    });
+  }
+};
+
+export default buildClient;
--- client/pages/index.js
+++ client/pages/index.js
@@ -1,4 +1,8 @@
-const HomePage = () => {
+import axios from 'axios';
+import buildClient from '../api/build-client';
+
+const HomePage = ({ currentUser }) => {
+  console.log(currentUser);
   return (
     <div>
       <h1>Big Ticket Items!</h1>
@@ -9,6 +13,19 @@ const HomePage = () => {
       </ul>
     </div>
   );
+};
+
+// `getInitialProps` enables server-side rendering in a page and allows you to do initial data population,
+// it means sending the page with the data already populated from the server. This is especially useful for SEO.
+// ? This method is called from server only when user makes refresh, or clicking link, or typing Ultrasonic
+// ? If this component is loaded from navigation, this function will be executed on the application (For example, after login)
+HomePage.getInitialProps = async (context) => {
+  const { data } = await buildClient(context)
+    .get()
+    .catch((err) => {
+      console.log(err.message);
+    });
+  return data;
 };
 
 export default HomePage;
