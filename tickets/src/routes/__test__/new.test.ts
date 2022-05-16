441ceababdcf99e5ca26430e2179cce2c8f9d88f
Sebastián Pérez Etchandy <sebastian@peretch.com>
2022-05-15 22:46:44 -0300

    [tickets] POST method finished with tdd approach

--- tickets/src/routes/__test__/new.test.ts
+++ tickets/src/routes/__test__/new.test.ts
@@ -1,5 +1,6 @@
 import request from 'supertest';
 import { app } from '../../app';
+import { Ticket } from '../../models/ticket';
 
 // if it returns 404, it fails
 it('has a route handler listening to /api/tickets for POST erquests', async () => {
@@ -13,6 +14,7 @@ it('Can only be accessed if user is signed in', async () => {
   expect(response.status).toEqual(401);
 });
 
+// This global.signin implementation will simulate all cookie creation
 it('returns status different to 401 if suer is signed in', async () => {
   const response = await request(app)
     .post('/api/tickets')
@@ -21,8 +23,63 @@ it('returns status different to 401 if suer is signed in', async () => {
   expect(response.status).not.toEqual(401);
 });
 
-it('returns an error if an invalid title is provided', async () => {});
+// Here we make use of RequestValidationError from common library
+it('returns an error if an invalid title is provided', async () => {
+  await request(app)
+    .post('/api/tickets')
+    .set('Cookie', global.signin())
+    .send({
+      title: '',
+      price: 10,
+    })
+    .expect(400);
 
-it('returns an error rif an invalid price is provided', async () => {});
+  await request(app)
+    .post('/api/tickets')
+    .set('Cookie', global.signin())
+    .send({
+      price: 10,
+    })
+    .expect(400);
+});
 
-it('creates a tickets with valid parameters', async () => {});
+it('returns an error rif an invalid price is provided', async () => {
+  await request(app)
+    .post('/api/tickets')
+    .set('Cookie', global.signin())
+    .send({
+      title: 'My valid title',
+      price: -10,
+    })
+    .expect(400);
+
+  await request(app)
+    .post('/api/tickets')
+    .set('Cookie', global.signin())
+    .send({
+      title: 'My valid title',
+    })
+    .expect(400);
+});
+
+it('creates a tickets with valid parameters', async () => {
+  const title = 'Coldplay concert in Buenos Aires';
+  const price = 20;
+  // This should be an empty array
+  let tickets = await Ticket.find({});
+  expect(tickets.length).toEqual(0);
+
+  await request(app)
+    .post('/api/tickets')
+    .set('Cookie', global.signin())
+    .send({
+      title,
+      price,
+    })
+    .expect(201);
+
+  tickets = await Ticket.find({});
+  expect(tickets.length).toEqual(1);
+  expect(tickets[0].price).toEqual(price);
+  expect(tickets[0].title).toEqual(title);
+});
